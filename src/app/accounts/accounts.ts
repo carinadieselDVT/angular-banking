14663d6c64041102d944262ec7e6b87960299e02
Carina Diesel <cdiesel@dvtsoftware.com>
2026-04-09 14:46:44 +0200

    feat: Added basic account card

--- src/app/accounts/accounts.ts
+++ src/app/accounts/accounts.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import bankdata from '../mockData.json';
+import { NgForOf } from '../../../node_modules/@angular/common/types/_common_module-chunk';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './accounts.html',
   styleUrl: './accounts.css',
 })
-export class Accounts {}
+export class Accounts {
+  accounts = bankdata;
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Accounts } from './accounts/accounts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Accounts,
+  },
+];
