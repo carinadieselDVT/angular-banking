9c292247583e4fc3c8b9f0a604e6e02b669d1746
Carina Diesel <cdiesel@dvtsoftware.com>
2026-04-10 12:55:20 +0200

    style: minor styling updates

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,20 +1,41 @@
 import { Attribute, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Account } from '../accounts/accounts.model';
+import {
+  MatCard,
+  MatCardHeader,
+  MatCardTitle,
+  MatCardSubtitle,
+  MatCardActions,
+} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardActions, MatButtonModule],
   template: `
-    <h3>Details</h3>
-    <h3>{{ accountName }}</h3>
-    <p>Bank: {{ account.bank_details.bank_name }}</p>
-    <p>Account #: {{ account.bank_details.account_number }}</p>
-    <p>Branch: {{ account.bank_details.branch_code }}</p>
-    <p>Type: {{ account.bank_details.account_type }}</p>
-    <button type="button" (click)="closeDetails()">Close</button>
+    <mat-card appearance="outlined">
+      <mat-card-header>
+        <mat-card-title>{{ accountName }}</mat-card-title>
+        <mat-card-subtitle>Bank: {{ account.bank_details.bank_name }}</mat-card-subtitle>
+        <mat-card-subtitle>Account #: {{ account.bank_details.account_number }}</mat-card-subtitle>
+        <mat-card-subtitle>Branch: {{ account.bank_details.branch_code }}</mat-card-subtitle>
+        <mat-card-subtitle>Type: {{ account.bank_details.account_type }}</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-actions>
+        <button mat-button (click)="closeDetails()">Close Details</button>
+      </mat-card-actions>
+    </mat-card>
   `,
-  styles: [],
+  styles: `
+    mat-card {
+      min-height: 100px;
+      width: 500px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  `,
 })
 export class DetailsComponent {
   // Account will always have a value. ! is a "pinky- promise" to typescript
